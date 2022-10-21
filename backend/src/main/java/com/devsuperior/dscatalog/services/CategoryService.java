0e33aace6f51ae473db68622ade8ab1cbdedaff5
Dyego Lima <dyegodll18@gmail.com>
2022-10-21 09:14:56 -0300

    DATABASE SEEDING

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -1,8 +1,11 @@
 package com.devsuperior.dscatalog.services;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
 
@@ -14,6 +17,8 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	//garante a transação com o banco e informa que é somente leitura para não travar o banco(lock)
+	@Transactional(readOnly = true) //obs.: import do Spring e não javax
 	public List<Category> findAll(){
 		return repository.findAll();
 	}

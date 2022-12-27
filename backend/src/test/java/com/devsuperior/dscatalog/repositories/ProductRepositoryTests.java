410721d879089409acfac57f9c1956891a4804e6
Dyego Lima <dyegodll18@gmail.com>
2022-12-27 12:45:57 -0300

    Fixtures + Test delete + Test save Product

---
+++ backend/src/test/java/com/devsuperior/dscatalog/repositories/ProductRepositoryTests.java
@@ -0,0 +1,56 @@
+package com.devsuperior.dscatalog.repositories;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.EmptyResultDataAccessException;
+
+import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.tests.ProductFactory;
+
+@DataJpaTest
+public class ProductRepositoryTests {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	private Long existingId;
+	private Long inexistingId;
+	private Long countTotalProduct = 25L;
+	
+	@BeforeEach
+	void setUp() throws Exception{
+		existingId = 1L;
+		inexistingId = 100L;
+	}
+	
+	@Test
+	public void deleteShouldDeleteObjectWhenIdExists() {
+		repository.deleteById(existingId);
+		Optional<Product> result = repository.findById(existingId);
+		Assertions.assertFalse(result.isPresent());
+	}
+
+	@Test
+	public void deleteShouldThrowEmptyResultDataAccessExceptionWhenInexistentId() {
+		Assertions.assertThrows(EmptyResultDataAccessException.class, ()->{
+			repository.deleteById(inexistingId);
+		});
+	}
+
+	@Test
+	public void saveShouldPersistWithAutoIncrementWhenIdIsNull() {
+		Product p = ProductFactory.createProduct();
+		p.setId(null);
+		
+		p = repository.save(p);
+		
+		Assertions.assertNotNull(p.getId());
+		Assertions.assertEquals(countTotalProduct + 1, p.getId());
+	}
+	
+}
---
+++ backend/src/test/java/com/devsuperior/dscatalog/tests/ProductFactory.java
@@ -0,0 +1,21 @@
+package com.devsuperior.dscatalog.tests;
+
+import java.time.Instant;
+
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.entities.Product;
+
+public class ProductFactory {
+
+	public static Product createProduct() {
+		Product p = new Product(1L, "PC Gamer Nitro", "Lorem ipsum dolor sit amet", 5799.90, "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/11-big.jpg", Instant.parse("2020-07-14T10:00:00Z"));
+		p.getCategories().add(new Category(3L,"Computers"));
+		return p;
+	}
+	
+	public static ProductDTO createProductDTO() {
+		Product prod = createProduct();
+		return new ProductDTO(prod, prod.getCategories());
+	}
+}

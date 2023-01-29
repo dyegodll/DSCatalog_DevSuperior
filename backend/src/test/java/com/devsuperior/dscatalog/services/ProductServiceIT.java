33ee4554ded2db53831bc0d1911d9b6fa6cdafa5
Dyego Lima <dyegodll18@gmail.com>
2023-01-29 18:00:03 -0300

    Teste de Integração - ProductService - findAllPaged - Ok!

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIT.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceIT.java
@@ -5,11 +5,17 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.ProductDTO;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @SpringBootTest
+@Transactional	//anotation para rollback a cada teste
 public class ProductServiceIT {
 
 	@Autowired
@@ -20,13 +26,42 @@ public class ProductServiceIT {
 	
 	private Long existingId;
 	private Long nonExistingId;
-	private Integer countTotalProducts;
+	private Long countTotalProducts;
 	
 	@BeforeEach
 	void setUp() throws Exception{
 		existingId = 1L;
 		nonExistingId = 1000L;
-		countTotalProducts = 25;
+		countTotalProducts = 25L;
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnSortedPageWhenSortByName() {
+		PageRequest page = PageRequest.of(0, 10, Sort.by("name")); //parâmetro pra ordenar a página por nome
+		Page<ProductDTO> result = service.findAllPaged(page);
+		Assertions.assertFalse(result.isEmpty());
+
+		//compara os 3 prineiros elementos da pág em ordem crescente
+		Assertions.assertEquals("Macbook Pro", result.getContent().get(0).getName());
+		Assertions.assertEquals("PC Gamer", result.getContent().get(1).getName());
+		Assertions.assertEquals("PC Gamer Alfa", result.getContent().get(2).getName());
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnPageEmptyWhenPageDoesNotExist() {
+		PageRequest page = PageRequest.of(50, 10); //classe concreta de Pageable
+		Page<ProductDTO> result = service.findAllPaged(page);
+		Assertions.assertTrue(result.isEmpty());
+	}
+	
+	@Test
+	public void findAllPagedShouldReturnPageWhenPage0Size10() {
+		PageRequest page = PageRequest.of(0, 10); //classe concreta de Pageable
+		Page<ProductDTO> result = service.findAllPaged(page);
+		Assertions.assertFalse(result.isEmpty());
+		Assertions.assertEquals(0, result.getNumber());
+		Assertions.assertEquals(10, result.getSize());
+		Assertions.assertEquals(countTotalProducts, result.getTotalElements());
 	}
 	
 	@Test

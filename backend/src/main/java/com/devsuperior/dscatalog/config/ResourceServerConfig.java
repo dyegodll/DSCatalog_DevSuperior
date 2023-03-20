5cd063cdf66bc76b86ff623ccd7a0df1996533da
Dyego Lima <dyegodll18@gmail.com>
2023-03-20 10:27:03 -0300

    H2 Security Configuration

--- backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.devsuperior.dscatalog.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -16,9 +19,13 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 	@Autowired
 	private JwtTokenStore tokenStore;
 	
+	//ambiente de desenvolvimento da aplicação
+	@Autowired
+	private Environment env;
+	
 	//enpoints perfil públicos
-	//tela de login
-	private static final String[] PUBLIC = {"/oauth/token"};
+	//1-tela de login,2-Console H2 Test
+	private static final String[] PUBLIC = {"/oauth/token","/h2-console/**"};
 	
 	//endpoints para perfil OPERADOR ou ADMIN
 	//todas as rotas a partir de products e categories
@@ -50,6 +57,14 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter{
 							.antMatchers(ADMIN).hasRole("ADMIN")//rotas liberadas para quem tiver perfil de ADMIN (restringe a rota somente a esse tipo de perfil)
 		//para acesso a qualquer outra rota, o usuário deve estar logado não importa o perfil atribuído a ele
 								.anyRequest().authenticated();
+		
+		//BANCO-H2 CONSOLE
+		//Quais os spring.profiles.active em execução?
+		//se detre os ativos existe o perfil teste
+		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			//desabilite a proteção dos frames dele
+			http.headers().frameOptions().disable();
+		}
 	}
 	
 }
